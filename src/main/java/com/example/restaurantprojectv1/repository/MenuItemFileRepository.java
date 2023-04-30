8433a7082601ea2c971ede309ef5fd65128d9541
Heesoo <soyamilk0705@naver.com>
2023-04-30 22:31:45 +0900

    repository 생성

---
+++ src/main/java/com/example/restaurantprojectv1/repository/MenuItemFileRepository.java
@@ -0,0 +1,16 @@
+package com.example.restaurantprojectv1.repository;
+
+import com.example.restaurantprojectv1.domain.entity.MenuItemFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MenuItemFileRepository extends JpaRepository<MenuItemFile, Long> {
+
+    Optional<MenuItemFile> findByFileName(String fileName);
+
+    void deleteByFileName(String fileName);
+
+}
---
+++ src/main/java/com/example/restaurantprojectv1/repository/ReviewFileRepository.java
@@ -0,0 +1,15 @@
+package com.example.restaurantprojectv1.repository;
+
+import com.example.restaurantprojectv1.domain.entity.ReviewFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReviewFileRepository extends JpaRepository<ReviewFile, Long> {
+
+    Optional<ReviewFile> findByFileName(String fileName);
+
+    void deleteByFileName(String fileName);
+}
