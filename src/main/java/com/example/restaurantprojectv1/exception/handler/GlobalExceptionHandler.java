2349b42feba256f1dc010c4d33d17205ef1c8c7c
Heesoo <soyamilk0705@naver.com>
2023-06-10 22:47:20 +0900

    사용하지 않는 exception 삭제

--- src/main/java/com/example/restaurantprojectv1/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/restaurantprojectv1/exception/handler/GlobalExceptionHandler.java
@@ -1,18 +1,13 @@
 package com.example.restaurantprojectv1.exception.handler;
 
-import com.example.restaurantprojectv1.exception.*;
+import com.example.restaurantprojectv1.exception.DataNotFoundException;
 import com.example.restaurantprojectv1.exception.dto.ErrorResponse;
-import com.example.restaurantprojectv1.exception.dto.ErrorObject;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,20 +21,4 @@ public class GlobalExceptionHandler {
                 httpServletRequest.getRequestURI()
         );
     }
-
-    @ExceptionHandler(ExistedDataException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleExistedDataException(ExistedDataException ex){
-        return ex.getMessage();
-    }
-
-    @ExceptionHandler(OverReservationPersons.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleOverReservationPersons(OverReservationPersons ex){
-        return ex.getMessage();
-    }
-
-
-
-
 }
